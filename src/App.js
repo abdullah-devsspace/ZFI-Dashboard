70118ef950ebb79ce0f023b239ea1181731d1c73
Muhammad zain <muhammad.zain@devsspace.com>
2023-10-09 12:59:03 +0500

    errors resolved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import './App.css';
+import 'jquery/dist/jquery.min.js'
+import $ from 'jquery';
+import jQuery from 'jquery';
+
+import './assets/vendor/chart.js/Chart.min.js'
+import './assets/css/sb-admin-2.css'
+import './assets/css/sb-admin-2.min.css'
+import './assets/vendor/bootstrap/js/bootstrap.bundle.js'
 import Home from './pages/Home';
 
 function App() {
